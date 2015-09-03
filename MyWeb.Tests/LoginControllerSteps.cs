e8d793b2a04ccc86b281b3233cba99292ac180db
Joey Chen <joeychen@yahoo-inc.com>
2015-09-03 18:36:05 +0800

    complete LoginController step definitions, and the "login success" of LoginController will pass because of the hard-code logic

--- MyWeb.Tests/LoginControllerSteps.cs
+++ MyWeb.Tests/LoginControllerSteps.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWeb.Controllers;
 using TechTalk.SpecFlow;
 
 namespace MyWeb.Tests.Steps
@@ -7,34 +10,53 @@ namespace MyWeb.Tests.Steps
 	[Scope(Feature = "LoginController")]
 	public class LoginControllerSteps
 	{
+		private LoginController _target;
+
+		[BeforeScenario]
+		public void BeforeScenario()
+		{
+			this._target = new LoginController();
+		}
+
 		[Given(@"login account is ""(.*)""")]
 		public void GivenLoginAccountIs(string account)
 		{
-			ScenarioContext.Current.Pending();
+			ScenarioContext.Current.Set<string>(account, "account");
 		}
 
 		[Given(@"user's password is ""(.*)""")]
 		public void GivenUserSPasswordIs(string password)
 		{
-			ScenarioContext.Current.Pending();
+			ScenarioContext.Current.Set<string>(password, "password");
 		}
 
 		[When(@"I invoke Index with HttpPost")]
 		public void WhenIInvokeIndexWithHttpPost()
 		{
-			ScenarioContext.Current.Pending();
+			var account = ScenarioContext.Current.Get<string>("account");
+			var password = ScenarioContext.Current.Get<string>("password");
+
+			var actual = this._target.Index(account, password);
+
+			ScenarioContext.Current.Set<ActionResult>(actual);
 		}
 
 		[Then(@"result's Controller name should be ""(.*)""")]
 		public void ThenResultSControllerNameShouldBe(string controllerName)
 		{
-			ScenarioContext.Current.Pending();
+			var actual = ScenarioContext.Current.Get<ActionResult>() as RedirectToRouteResult;
+
+			Assert.IsNotNull(actual);
+			Assert.AreEqual(controllerName, actual.RouteValues["Controller"]);
 		}
 
 		[Then(@"result's Action name should be ""(.*)""")]
 		public void ThenResultSActionNameShouldBe(string actionName)
 		{
-			ScenarioContext.Current.Pending();
+			var actual = ScenarioContext.Current.Get<ActionResult>() as RedirectToRouteResult;
+
+			Assert.IsNotNull(actual);
+			Assert.AreEqual(actionName, actual.RouteValues["Action"]);
 		}
 	}
 }
