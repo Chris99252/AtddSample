bff682728dd67e530d251bdbda8db28e1f471e3a
Joey Chen <joeychen@yahoo-inc.com>
2015-09-03 19:13:10 +0800

    產生 MyHash 與 ProfileDao class

---
+++ MyWeb/Models/MyHash.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyWeb.Models
+{
+	public class MyHash : IHash
+	{
+		public string GetHash(string original)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ MyWeb/Models/ProfileDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyWeb.Models
+{
+	public class ProfileDao : IProfileDao
+	{
+		public string GetPassword(string id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
