e7925d7eddff559aba3686cc62b7ea9ff244569e
Joey Chen <joeychen@yahoo-inc.com>
2015-09-03 18:56:54 +0800

    從 LoginController 的 AuthService property getter 產生 AuthService, 並實作 IAuth; 建置成功

---
+++ MyWeb/Models/AuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyWeb.Models
+{
+	public class AuthService : IAuth
+	{
+		public bool Validate(string account, string password)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
