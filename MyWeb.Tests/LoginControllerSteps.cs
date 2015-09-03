31a5d11e2aea69efd9e5212f69c0c64afb56e08c
Joey Chen <joeychen@yahoo-inc.com>
2015-09-03 18:33:45 +0800

    generating steps and modify parameter name

---
+++ MyWeb.Tests/LoginControllerSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MyWeb.Tests.Steps
+{
+	[Binding]
+	[Scope(Feature = "LoginController")]
+	public class LoginControllerSteps
+	{
+		[Given(@"login account is ""(.*)""")]
+		public void GivenLoginAccountIs(string account)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Given(@"user's password is ""(.*)""")]
+		public void GivenUserSPasswordIs(string password)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I invoke Index with HttpPost")]
+		public void WhenIInvokeIndexWithHttpPost()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"result's Controller name should be ""(.*)""")]
+		public void ThenResultSControllerNameShouldBe(string controllerName)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"result's Action name should be ""(.*)""")]
+		public void ThenResultSActionNameShouldBe(string actionName)
+		{
+			ScenarioContext.Current.Pending();
+		}
+	}
+}
