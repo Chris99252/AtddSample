c088951ffaa3c165a12678cac65c8f62c55458d0
Joey Chen <joeychen@yahoo-inc.com>
2015-09-03 18:15:21 +0800

    add LoginController and WelcomeController, hard-code logic and fake data for passing the "login success" scenario.

---
+++ MyWeb/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public ActionResult Index(string account, string password)
+		{
+			if (account == "joeychen" && password == "1234")
+			{
+				return RedirectToAction("Index", "Welcome");
+			}
+
+			return View();
+		}
+    }
+}
